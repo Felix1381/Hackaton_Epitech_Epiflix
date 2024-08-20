7edb0aa43f0d176996c5f479bd19daa2c5451f8f
Felix1381 <felix.tete@epitech.eu>
2024-08-20 21:02:29 +0000

    [INIT] project Vue3

---
+++ epiflix/src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./assets/tailwind.css";
+import { createPinia } from "pinia";
+import "./assets/tailwind.css";
+
+createApp(App).use(createPinia()).use(router).mount("#app");
